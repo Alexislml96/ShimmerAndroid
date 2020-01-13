9d30392f8144922fc488a571a631e74df2a0e25c
Alexislml96 <32379788+Alexislml96@users.noreply.github.com>
2020-01-12 22:14:21 -0800

    Inserción inicial en el repositorio del módulo Loader

---
+++ Loader/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Android.Animation;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Facebook.Shimmer;
+using Java.Lang;
+
+namespace Loader
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private ShimmerFrameLayout mShimmerViewContainer;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            mShimmerViewContainer = FindViewById<ShimmerFrameLayout>(Resource.Id.shimmer_loader);
+            mShimmerViewContainer.StartShimmer();
+        }
+    }
+}
+
+
